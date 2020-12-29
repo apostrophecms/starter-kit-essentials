a03d2419ecd4434512c346212b19ab3d4fb4718b
Tom Boutell <tom@apostrophecms.com>
2020-12-29 11:53:31 -0500

    set the webpack mode properly

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  mode: process.env.NODE_ENV || 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'modules/asset/ui/public'),
