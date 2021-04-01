9cd149c739e244571f91695ade125265fa1f6493
Alex Bea <abea@mailbox.org>
2021-04-01 11:04:48 -0500

    Polishes boilerplate app.js comments and moves express

--- app.js
+++ app.js
@@ -2,27 +2,33 @@ const path = require('path');
 
 require('apostrophe')({
   shortName: 'a3-boilerplate',
-
   modules: {
-
     // Apostrophe module configuration
-
-    // Note: most configuration occurs in the respective
-    // modules' directories. See lib/@apostrophecms/assets/index.js for an example.
-
-    // However any modules that are not present by default in Apostrophe must at
-    // least have a minimal configuration here to turn them on: `moduleName: {}`
-
-    // If a template is not found somewhere else, serve it from the top-level
-    // `views/` folder of the project
-
+    // *******************************
+    //
+    // NOTE: most configuration occurs in the respective modules' directories.
+    // See modules/@apostrophecms/page/index.js for an example.
+    //
+    // Any modules that are not present by default in Apostrophe must at least
+    // have a minimal configuration here to turn them on: `moduleName: {}`
+    // ***********************************************************************
+    '@apostrophecms/express': {
+      options: {
+        session: {
+          // ⚠️ If this still says `undefined`, set a real secret (a random
+          // string)!
+          secret: undefined
+        }
+      }
+    },
     '@apostrophecms/template': {
       options: {
+        // If a template is not found somewhere else, serve it from the
+        // root-level `views/` directory of the project.
         viewsFolderFallback: path.join(__dirname, 'views')
       }
     },
-
-    // Custom CSS classes for standard apostrophe widgets
+    // `className` options set custom CSS classes for Apostrophe core widgets.
     '@apostrophecms/rich-text-widget': {
       options: {
         className: 'bp-rich-text'
@@ -38,17 +44,10 @@ require('apostrophe')({
         className: 'bp-video-widget'
       }
     },
-    '@apostrophecms/express': {
-      options: {
-        session: {
-          // If this still says `undefined`, set a real secret!
-          secret: undefined
-        }
-      }
-    },
 
-    // A home for our own project-specific javascript and SASS assets
+    // `asset` supports the project's webpack build for client-side assets.
     asset: {},
+    // The project's first custom page type.
     'default-page': {}
 
   }
