1231fbd3dccd13f1aad8d9d08344945247efa427
Alex Bea <abea@mailbox.org>
2021-05-14 16:11:23 -0500

    Cleans up app.js and adds a dev checklist

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const path = require('path');
-
 require('apostrophe')({
   shortName: 'a3-boilerplate',
   modules: {
@@ -12,22 +10,6 @@ require('apostrophe')({
     // Any modules that are not present by default in Apostrophe must at least
     // have a minimal configuration here to turn them on: `moduleName: {}`
     // ***********************************************************************
-    '@apostrophecms/express': {
-      options: {
-        session: {
-          // ⚠️ If this still says `undefined`, set a real secret (a random
-          // string)!
-          secret: undefined
-        }
-      }
-    },
-    '@apostrophecms/template': {
-      options: {
-        // If a template is not found somewhere else, serve it from the
-        // root-level `views/` directory of the project.
-        viewsFolderFallback: path.join(__dirname, 'views')
-      }
-    },
     // `className` options set custom CSS classes for Apostrophe core widgets.
     '@apostrophecms/rich-text-widget': {
       options: {
@@ -44,11 +26,9 @@ require('apostrophe')({
         className: 'bp-video-widget'
       }
     },
-
     // `asset` supports the project's webpack build for client-side assets.
     asset: {},
     // The project's first custom page type.
     'default-page': {}
-
   }
 });
