f4453472d0f8ccd67fd79e13f1800964b27d7094
Miro Yovchev <2827783+myovchev@users.noreply.github.com>
2024-05-28 14:07:45 +0300

    SEO Assistant setup

--- app.js
+++ app.js
@@ -29,6 +29,22 @@ require('apostrophe')({
     // `asset` supports the project's webpack build for client-side assets.
     asset: {},
     // The project's first custom page type.
-    'default-page': {}
+    'default-page': {},
+    '@apostrophecms-pro/automatic-translation': {
+      options: {
+        enabled: false
+      }
+    },
+    '@apostrophecms/seo': {},
+    '@apostrophecms-pro/seo-assistant': {
+      options: {
+        provider: 'openai'
+      }
+    },
+    '@apostrophecms-pro/seo-assistant-openai': {
+      options: {
+        apiKey: process.env.OPENAI_API_KEY
+      }
+    }
   }
 });
