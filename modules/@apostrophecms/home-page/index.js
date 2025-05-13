011e0967312994aa2fb134a1b5b29afa1c8e70a8
Jed <vjeudy@protonmail.com>
2025-05-13 16:47:16 +0200

    removes all specific rich-text configs$

--- modules/@apostrophecms/home-page/index.js
+++ modules/@apostrophecms/home-page/index.js
@@ -8,41 +8,7 @@ export default {
         type: 'area',
         options: {
           widgets: {
-            '@apostrophecms/rich-text': {
-              toolbar: [
-                'styles',
-                '|',
-                'bold',
-                'italic',
-                'strike',
-                'link',
-                '|',
-                'bulletList',
-                'orderedList',
-                '|',
-                'table',
-                'image'
-              ],
-              styles: [
-                {
-                  tag: 'p',
-                  label: 'Paragraph (P)'
-                },
-                {
-                  tag: 'h3',
-                  label: 'Heading 3 (H3)'
-                },
-                {
-                  tag: 'h4',
-                  label: 'Heading 4 (H4)'
-                }
-              ],
-              insert: [
-                'table',
-                'importTable',
-                'image'
-              ]
-            },
+            '@apostrophecms/rich-text': {},
             '@apostrophecms/image': {},
             '@apostrophecms/video': {}
           }
