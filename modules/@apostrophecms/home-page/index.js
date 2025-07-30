4d118af3582aabd764530a8a454d2827d483b7d2
Thomas Boutell <tom@apostrophecms.com>
2025-07-30 09:24:41 -0400

    example of p with multiple classes for testing

--- modules/@apostrophecms/home-page/index.js
+++ modules/@apostrophecms/home-page/index.js
@@ -8,7 +8,42 @@ export default {
         type: 'area',
         options: {
           widgets: {
-            '@apostrophecms/rich-text': {},
+            '@apostrophecms/rich-text': {
+              styles: [
+                {
+                  tag: 'p',
+                  class: 'red',
+                  label: 'Red P (Default, Edge Case)'
+                },
+                {
+                  tag: 'p',
+                  label: 'Plain Paragraph'
+                },
+                {
+                  tag: 'p',
+                  class: 'green',
+                  label: 'Green P'
+                },
+                {
+                  tag: 'h1',
+                  label: 'H1'
+                },
+                {
+                  tag: 'h2',
+                  label: 'H2'
+                },
+                {
+                  tag: 'h2',
+                  class: 'red',
+                  label: 'H2 Red'
+                },
+                {
+                  tag: 'h2',
+                  class: 'green',
+                  label: 'H2 Green'
+                }
+              ]
+            },
             '@apostrophecms/image': {},
             '@apostrophecms/video': {}
           }
