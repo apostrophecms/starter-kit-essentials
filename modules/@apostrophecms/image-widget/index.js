afc4b779cb393f605342f470e54ff33071bd21b9
Miro Yovchev <2827783+myovchev@users.noreply.github.com>
2025-09-11 12:45:44 +0300

    Disable additional BC operations

--- modules/@apostrophecms/image-widget/index.js
+++ modules/@apostrophecms/image-widget/index.js
@@ -2,14 +2,14 @@ export default {
   // Showcase standard widget actions in the breadcrumb
   widgetOperations(self, options) {
     return {
-      add: {
-        editCustom: {
-          placement: 'breadcrumb',
-          action: 'edit',
-          icon: 'cog-icon',
-          tooltip: 'apostrophe:edit'
-        }
-      }
+      // add: {
+      //   editCustom: {
+      //     placement: 'breadcrumb',
+      //     action: 'edit',
+      //     icon: 'cog-icon',
+      //     tooltip: 'apostrophe:edit'
+      //   }
+      // }
     };
   }
 };
--- modules/@apostrophecms/rich-text-widget/index.js
+++ modules/@apostrophecms/rich-text-widget/index.js
@@ -2,14 +2,14 @@ export default {
   // Showcase the standard widget actions in the breadcrumb
   widgetOperations(self, options) {
     return {
-      add: {
-        deleteCustom: {
-          placement: 'breadcrumb',
-          action: 'remove',
-          icon: 'delete-icon',
-          tooltip: 'apostrophe:delete'
-        }
-      }
+      // add: {
+      //   deleteCustom: {
+      //     placement: 'breadcrumb',
+      //     action: 'remove',
+      //     icon: 'delete-icon',
+      //     tooltip: 'apostrophe:delete'
+      //   }
+      // }
     };
   }
 };
