03f05a3e1fb57afcbe0b1bb9c058b7392e8e2627
Miro Yovchev <2827783+myovchev@users.noreply.github.com>
2025-10-21 10:49:36 +0300

    Add layout widget support and enhance page structure
    - Introduced '@apostrophecms/layout' widget in home and default page areas.
    - Updated layout styles to allow for a wider main content area.
    - Improved HTML structure for better layout management.

--- modules/default-page/index.js
+++ modules/default-page/index.js
@@ -9,19 +9,28 @@ export default {
         type: 'area',
         options: {
           widgets: {
+            '@apostrophecms/layout': {},
+          }
+        }
+      },
+      content: {
+        type: 'area',
+        options: {
+          widgets: {
             '@apostrophecms/rich-text': {},
             '@apostrophecms/image': {},
             '@apostrophecms/video': {}
           }
         }
-      }
+      },
     },
     group: {
       basics: {
         label: 'Basics',
         fields: [
           'title',
-          'main'
+          'main',
+          'content'
         ]
       }
     }
