752def4aa1aa72b747ab98176c07924731125649
ETLaurent <git.sabbath464@passmail.net>
2025-12-10 15:52:04 +0100

    applying widget level styles in template

---
+++ modules/my-widget/index.js
@@ -0,0 +1,41 @@
+export default {
+  extend: '@apostrophecms/widget-type',
+  options: {
+    label: 'My Widget'
+    // stylesWrapper: false
+  },
+  styles(self, options) {
+    console.log('options.myGenericColor', options.myGenericColor);
+    return {
+      add: {
+        myBorder: 'border',
+        alignment: 'alignment',
+        backgroundColor: {
+          type: 'color',
+          property: 'background-color'
+        }
+      }
+    };
+  },
+  fields: {
+    add: {
+      title: {
+        type: 'string',
+        label: 'My Title'
+      },
+      image: {
+        type: 'area',
+        label: 'My Image',
+        max: 1,
+        options: {
+          widgets: {
+            '@apostrophecms/image': {
+              aspectRatio: [ 16, 9 ],
+              minSize: [ 1200, 675 ]
+            }
+          }
+        }
+      }
+    }
+  }
+};
