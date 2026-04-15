0578351a67d498e9a5c9130637b409a37ff94f3b
Miro Yovchev <2827783+myovchev@users.noreply.github.com>
2026-04-15 14:02:18 +0300

    Showcase content shown in layout column styles editor and opt-out of recently added.

--- modules/@apostrophecms/layout-column-widget/index.js
+++ modules/@apostrophecms/layout-column-widget/index.js
@@ -1,4 +1,9 @@
 export default {
+  options: {
+    // Do not display tabs when there is only one.
+    // Since there is only one Styles tab, it will be hidden.
+    hideSingleTab: true
+  },
   styles: {
     add: {
       background: 'background'
@@ -16,6 +21,13 @@ export default {
             'nested-layout': {}
           }
         }
+      }
+    },
+    // Let the content be shown in the Styles tab schema.
+    // Keep in mind this is only a visual change, no layout logic is affected.
+    group: {
+      styles: {
+        fields: [ 'content' ]
       }
     }
   }
